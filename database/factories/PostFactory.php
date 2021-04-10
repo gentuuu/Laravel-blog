1219784bf7e9332779de6cb8b67cb389a01f152d
gentuuu <gentuuu1993@gmail.com>
2021-04-10 15:06:03 +0200

    init

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class PostFactory extends Factory
+
+
+{
+
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    // public function definition()
+    // {
+    //     return [
+    //         'title' => $this->faker->sentence(5),
+    //         'content' => $this->faker->paragraph(5),
+    //         'date' => now(),
+    //         'type' => 'text'
+    //     ];
+    // }
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(5),
+            'content' => null,
+            'type' => 'photo',
+            'image' => $this->faker->imageUrl(1200,800)
+        ];
+    }
+
+
+}
